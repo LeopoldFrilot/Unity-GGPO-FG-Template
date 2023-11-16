5aceb3dad2623882d0eb8e79e3e0917a4c82eb6e
LeopoldFrilot <LAFrilot@outlook.com>
2023-11-16 01:00:33 -0800

    GGPO Mirror

---
+++ Unity/Assets/VectorWar/Runtime/Scripts/VwGameView.cs
@@ -0,0 +1,59 @@
+﻿using SharedGame;
+using System;
+using UnityEngine;
+
+namespace VectorWar {
+
+    public class VwGameView : MonoBehaviour, IGameView {
+        public VwShipView shipPrefab;
+        public Transform bulletPrefab;
+
+        private VwShipView[] shipViews = Array.Empty<VwShipView>();
+        private Transform[][] bulletLists;
+        private GameManager gameManager => GameManager.Instance;
+
+        private void ResetView(VwGame gs) {
+            var shipGss = gs._ships;
+            shipViews = new VwShipView[shipGss.Length];
+            bulletLists = new Transform[shipGss.Length][];
+
+            for (int i = 0; i < shipGss.Length; ++i) {
+                shipViews[i] = Instantiate(shipPrefab, transform);
+                bulletLists[i] = new Transform[shipGss[i].bullets.Length];
+                for (int j = 0; j < bulletLists[i].Length; ++j) {
+                    bulletLists[i][j] = Instantiate(bulletPrefab, transform);
+                }
+            }
+        }
+
+        public void UpdateGameView(IGameRunner runner) {
+            var gs = (VwGame)runner.Game;
+            var ngs = runner.GameInfo;
+
+            var shipsGss = gs._ships;
+            if (shipViews.Length != shipsGss.Length) {
+                ResetView(gs);
+            }
+            for (int i = 0; i < shipsGss.Length; ++i) {
+                shipViews[i].Populate(shipsGss[i], ngs.players[i]);
+                UpdateBullets(shipsGss[i].bullets, bulletLists[i]);
+            }
+        }
+
+        private void UpdateBullets(Bullet[] bullets, Transform[] bulletList) {
+            for (int j = 0; j < bulletList.Length; ++j) {
+                bulletList[j].position = bullets[j].position;
+                if (bullets[j].velocity.sqrMagnitude > Mathf.Epsilon) {
+                    bulletList[j].rotation = Quaternion.LookRotation(bullets[j].velocity, Vector3.up);
+                }
+                bulletList[j].gameObject.SetActive(bullets[j].active);
+            }
+        }
+
+        private void Update() {
+            if (gameManager.IsRunning) {
+                UpdateGameView(gameManager.Runner);
+            }
+        }
+    }
+}
---
+++ bin/start_vectorwar_1p_p2.cmd
@@ -0,0 +1,11 @@
+REM Test the vectorwar sample by starting 2 clients connected
+REM back to each other.
+REM
+REM Controls: Arrows to move
+REM           Press 'D' to fire
+REM           Press 'P' to show performance monitor
+REM           Shift to strafe
+
+pushd ..\build\bin\x64\Debug
+start VectorWar.exe 7001 2 127.0.0.1:7000 local
+popd
